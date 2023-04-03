4c8f608453936b41776d12982f2ef586bc89c150
inyestar <inyestar@techtaka.com>
2023-04-04 08:41:23 +0900

    [netty-test] divided modules

---
+++ echo-server/src/main/java/org/inyestar/netty/echoserver/handler/EchoServerHandler.java
@@ -0,0 +1,28 @@
+package org.inyestar.netty.echoserver.handler;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.util.CharsetUtil;
+
+@ChannelHandler.Sharable
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ByteBuf in = (ByteBuf) msg;
+        System.out.printf("[CLIENT-%s] %s%n", ctx.channel().id(), in.toString(CharsetUtil.UTF_8));
+        ctx.write(in);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) {
+        ctx.writeAndFlush(Unpooled.EMPTY_BUFFER).addListener(ChannelFutureListener.CLOSE);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable e) {
+        e.printStackTrace();
+        ctx.close();
+    }
+}
