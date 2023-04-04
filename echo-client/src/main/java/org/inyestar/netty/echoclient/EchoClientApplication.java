c42b4ac79ecf156b3c1a5d5505a1005cc59f3dc4
inyestar <inyestar@techtaka.com>
2023-04-04 09:06:15 +0900

    [netty-test] add echo client module

---
+++ echo-client/src/main/java/org/inyestar/netty/echoclient/EchoClientApplication.java
@@ -0,0 +1,13 @@
+package org.inyestar.netty.echoclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchoClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EchoClientApplication.class, args);
+    }
+
+}
---
+++ echo-client/src/test/java/org/inyestar/netty/echoclient/EchoClientApplicationTests.java
@@ -0,0 +1,13 @@
+package org.inyestar.netty.echoclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchoClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
