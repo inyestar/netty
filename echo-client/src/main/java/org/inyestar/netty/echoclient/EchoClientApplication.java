4d488f46acec3b53b10da51f88e9b7ed617d85d5
inyestar <inyestar@techtaka.com>
2023-04-04 09:40:21 +0900

    [netty-test] added gradle config

--- echo-client/src/main/java/org/inyestar/netty/echoclient/EchoClientApplication.java
+++ echo-client/src/main/java/org/inyestar/netty/echoclient/EchoClientApplication.java
@@ -1,13 +1,10 @@
 package org.inyestar.netty.echoclient;
 
 import org.inyestar.netty.echoclient.client.EchoClient;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class EchoClientApplication {
 
     public static void main(String[] args) throws Exception {
-//        SpringApplication.run(EchoClientApplication.class, args);
         if (args.length != 2) {
             System.err.printf("Usage : %s <host> <port>", EchoClient.class.getSimpleName());
         }
--- echo-server/src/main/java/org/inyestar/netty/echoserver/EchoServerApplication.java
+++ echo-server/src/main/java/org/inyestar/netty/echoserver/EchoServerApplication.java
@@ -1,13 +1,10 @@
 package org.inyestar.netty.echoserver;
 
 import org.inyestar.netty.echoserver.server.EchoServer;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class EchoServerApplication {
 
     public static void main(String[] args) throws Exception {
-//        SpringApplication.run(EchoServerApplication.class, args);
         if (args.length != 1) {
             System.err.printf("Usage : %s <port>", EchoServer.class.getSimpleName());
         }
